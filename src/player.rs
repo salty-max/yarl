98f553503ed1e9bc5d3c70ce5c097bc870b03554
Maxime Blanc <max@jellycat.fr>
2021-04-27 21:54:41 +0200

    Graphics, Camera, Action (#13)
    * font files
    
    * color palette
    
    * Font + camera following player

--- src/player.rs
+++ src/player.rs
@@ -9,17 +9,18 @@ impl Player {
         Player { position }
     }
 
-    pub fn render(&self, ctx: &mut BTerm) {
+    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
+        ctx.set_active_console(1);
         ctx.set(
-            self.position.x,
-            self.position.y,
-            WHITE,
-            BLACK,
+            self.position.x - camera.left_x,
+            self.position.y - camera.top_y,
+            color_from_palette("yellow").unwrap(),
+            color_from_palette("black").unwrap(),
             to_cp437('@'),
         )
     }
 
-    pub fn update(&mut self, ctx: &mut BTerm, map: &Map) {
+    pub fn update(&mut self, ctx: &mut BTerm, map: &Map, camera: &mut Camera) {
         if let Some(key) = ctx.key {
             let delta = match key {
                 VirtualKeyCode::Left => Point::new(-1, 0),
@@ -32,6 +33,7 @@ impl Player {
             let new_position = self.position + delta;
             if map.can_enter_tile(new_position) {
                 self.position = new_position;
+                camera.on_player_move(new_position);
             }
         }
     }
