978f639463fab0200bf77654cc03eaf0fda85201
Maxime Blanc <max@jellycat.fr>
2021-04-27 16:55:58 +0200

    color palette

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 mod map;
 mod map_builder;
+mod palette;
 mod player;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+    pub const TILE_SIZE: i32 = 16;
     pub use crate::map::*;
     pub use crate::map_builder::*;
+    pub use crate::palette::*;
     pub use crate::player::*;
 }
 
@@ -39,9 +44,15 @@ impl GameState for State {
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
+    let context = BTermBuilder::new()
         .with_title("YARL")
         .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(TILE_SIZE, TILE_SIZE)
+        .with_resource_path("resources")
+        .with_font("pico16s.png", TILE_SIZE, TILE_SIZE)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "pico16s.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "pico16s.png")
         .build()?;
 
     main_loop(context, State::new())
---
+++ src/palette.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+
+use crate::prelude::*;
+
+pub fn color_from_palette(c: &str) -> Result<RGB, String> {
+    let mut palette = HashMap::new();
+    palette.insert("black", "#000000");
+    palette.insert("dark_blue", "#1D2B53");
+    palette.insert("dark_purple", "#7E2553");
+    palette.insert("dark_green", "#008751");
+    palette.insert("brown", "#AB5236");
+    palette.insert("dark_grey", "#5F574F");
+    palette.insert("light_grey", "#C2C3C7");
+    palette.insert("dark_grey", "#5F574F");
+    palette.insert("white", "#FFF1E8");
+    palette.insert("red", "#FF004D");
+    palette.insert("orange", "#FFA300");
+    palette.insert("yellow", "#FFEC27");
+    palette.insert("green", "#00E436");
+    palette.insert("blue", "#29ADFF");
+    palette.insert("lavender", "#83769C");
+    palette.insert("pink", "#FF77A8");
+    palette.insert("light_peach", "#FFCCAA");
+
+    let mut color: Option<RGB> = None;
+
+    for (k, _) in palette.iter() {
+        if &c == k {
+            color = Some(RGB::from_hex(&c).unwrap());
+        }
+    }
+
+    match color {
+        Some(color) => Ok(color),
+        None => Err(String::from("Invalid color")),
+    }
+}
